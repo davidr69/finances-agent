f0241cb86973b49d561cb595efd93e5c9ba5ac55
David Rosario <david@lavacro.com>
2026-06-10 10:52:58 -0400

    temporarily logging and writing file to validate; confirmed that messages are being consumed properly

--- src/main/java/net/lavacro/finances/agent/kafka/MyConsumer.java
+++ src/main/java/net/lavacro/finances/agent/kafka/MyConsumer.java
@@ -2,19 +2,37 @@ package net.lavacro.finances.agent.kafka;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.header.Header;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @Component
 public class MyConsumer {
 
-	@KafkaListener(topics = "finances-topic", batch = "true")
-	public void listen(List<ConsumerRecord<String, byte[]>> messages) {
-		messages.forEach(m -> {
-			log.info("Received record: {}", m);
-		});
+	@KafkaListener(topics = "finances-topic")
+	public void listen(ConsumerRecord<String, byte[]> message) {
+		log.info("Received record: {}", message);
+
+		Header filenameHeader = message.headers().lastHeader("filename");
+		String filename = filenameHeader != null ? new String(filenameHeader.value(), StandardCharsets.UTF_8) : null;
+		log.info("file name: {}", filename);
+
+		Header accountHeader = message.headers().lastHeader("accountId");
+		String accountId = accountHeader != null ? new String(accountHeader.value(), StandardCharsets.UTF_8) : null;
+		log.info("account id: {}", accountId);
+
+		byte[] payload = message.value();
+		File file = new File("/tmp/temp.pdf");
+		try (FileOutputStream fos = new FileOutputStream(file)) {
+			fos.write(payload);
+		} catch (IOException e) {
+			log.error("Error writing file: {}", e.getMessage());
+		}
 	}
 }
